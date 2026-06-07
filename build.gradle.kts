9f98285e7067cfb87080d9fb5466399153c0b2fa
Dyno <85120286+dynodevv@users.noreply.github.com>
2026-06-07 20:25:56 +0200

    fix(build): use buildscript classpath for KSP, serialization, and compose plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
 // Top-level build file
+buildscript {
+    dependencies {
+        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.3.9")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:2.3.0")
+        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.3.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.11.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("com.google.devtools.ksp") version "2.3.9" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
 }
