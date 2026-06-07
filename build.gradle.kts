f5332f5af344edb4f9bd0211545a0d3649087a9b
Dyno <85120286+dynodevv@users.noreply.github.com>
2026-06-07 20:22:32 +0200

    fix(build): use KSP 2.3.9 (independent versioning) for Kotlin 2.3.0 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.11.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("com.google.devtools.ksp") version "2.3.0-1.0.28" apply false
+    id("com.google.devtools.ksp") version "2.3.9" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
 }
